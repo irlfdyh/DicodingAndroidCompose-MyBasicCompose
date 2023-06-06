58d11de778621663d016666d24618f3915b6fd6e
Irshal Firdeansyah <irshalfir@gmail.com>
2023-06-06 08:38:07 +0700

    update greeting item to hold expand state

--- app/src/main/java/com/dicoding/compose/mbc/MainActivity.kt
+++ app/src/main/java/com/dicoding/compose/mbc/MainActivity.kt
@@ -11,10 +11,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExpandLess
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -72,6 +81,9 @@ fun GreetingList(names: List<String>) {
 
 @Composable
 fun Greeting(name: String) {
+
+    var isExpanded by remember { mutableStateOf(false) }
+
     Row(
         modifier = Modifier.padding(8.dp),
         verticalAlignment = Alignment.CenterVertically
@@ -81,7 +93,9 @@ fun Greeting(name: String) {
             contentDescription = "Logo Jetpack Compose",
             modifier = Modifier.size(80.dp)
         )
-        Column {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
             Text(
                 text = "Hello $name!",
                 fontSize = 24.sp,
@@ -89,6 +103,12 @@ fun Greeting(name: String) {
             )
             Spacer(modifier = Modifier.width(8.dp))
             Text(text = "Welcome to Dicoding!")
+        }
+        IconButton(onClick = { isExpanded = !isExpanded }) {
+            Icon(
+                imageVector = if (isExpanded) Icons.Outlined.ExpandLess else Icons.Outlined.ExpandMore,
+                contentDescription =  if(isExpanded) "Show Less" else "Show More"
+            )
         }
     }
 }
